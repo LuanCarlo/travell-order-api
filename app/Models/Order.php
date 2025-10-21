19b8f7a34d9c570478e852cbbdb5ecaac8285a1f
LuanCarlo <luancarlo.paulo@gmail.com>
2025-10-20 22:00:44 -0300

    Criação de migrations e models iniciais

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrderFactory> */
+    use HasFactory;
+
+    protected $table = 'order';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+}
---
+++ database/migrations/2025_10_20_185720_create_order_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+     /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_status', function (Blueprint $table) {
+            $table->id();
+            $table->string('status');
+            $table->timestamps();
+        });
+
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination');
+            $table->dateTime('departure_date');
+            $table->dateTime('return_date');
+            $table->biginteger('order_status_id')->unsigned();
+            $table->biginteger('user_id')->unsigned();
+            $table->foreign('order_status_id')->references('id')->on('order_status')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        DB::table('order_status')->insert([
+            ['status' => 'Pendente',  'created_at' => now(), 'updated_at' => now()],
+            ['status' => 'Aprovado',  'created_at' => now(), 'updated_at' => now()],
+            ['status' => 'Cancelado', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+        Schema::dropIfExists('order_status');
+    }
+};
