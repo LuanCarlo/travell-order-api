b2ca6a13cf052a455ee9d0e6778ec2c1f331adc3
LuanCarlo <luancarlo.paulo@gmail.com>
2025-10-23 19:17:59 -0300

    alteração de atenticação

--- routes/api.php
+++ routes/api.php
@@ -11,14 +11,16 @@ Route::get('/ping', function () {
 Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
 
-Route::prefix('/orders')->group(function () {
-    Route::get('/', [App\Http\Controllers\OrderController::class, 'index']);
-    Route::post('/', [App\Http\Controllers\OrderController::class, 'store']);
-    Route::get('/getOrder/{id}', [App\Http\Controllers\OrderController::class, 'show']);
-    Route::put('/update/{id}', [App\Http\Controllers\OrderController::class, 'update']);
+Route::middleware('auth:api')->group(function () {
+    Route::prefix('/orders')->group(function () {
+        Route::get('/', [App\Http\Controllers\OrderController::class, 'index']);
+        Route::post('/', [App\Http\Controllers\OrderController::class, 'store']);
+        Route::get('/getOrder/{id}', [App\Http\Controllers\OrderController::class, 'show']);
+        Route::put('/update/{id}', [App\Http\Controllers\OrderController::class, 'update']);
 
-    Route::patch('/updateStatusOrder/{id}', [App\Http\Controllers\OrderController::class, 'updateStatusOrder']);
+        Route::patch('/updateStatusOrder/{id}', [App\Http\Controllers\OrderController::class, 'updateStatusOrder']);
 
-    Route::get('/orderSatus', [App\Http\Controllers\OrderStatusController::class, 'index']);
+        Route::get('/orderSatus', [App\Http\Controllers\OrderStatusController::class, 'index']);
 
+    });
 });
