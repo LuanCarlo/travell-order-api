76122478622be1019c2b5ebe9fc468862c8db407
LuanCarlo <luancarlo.paulo@gmail.com>
2025-10-24 02:44:35 -0300

    remoção de rota desnecessária

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 
-
-Route::get('/ping', function () {
-    return response()->json(['message' => 'API ativa! 🚀']);
-});
-
 Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
 
